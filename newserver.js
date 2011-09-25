f7f3450965748a234fc03bb7a68f356b8152e733
Jeremy Fleischman <jeremyfleischman@gmail.com>
2011-09-25 05:13:28 -0700

    Rewriting server/client with nowjs. This is easy mode.

---
+++ newserver.js
@@ -0,0 +1,186 @@
+var paperboy = require('paperboy');
+var nowjs = require('now');
+var url = require("url");
+var sys = require('sys');
+var assert = require('assert');
+
+//Object.prototype.keys = function() {
+function keys(obj) {
+	var keys = [];
+	for(var key in obj) {
+		if(obj.hasOwnProperty(key)) {
+			keys.push(key);
+		}
+	}
+	return keys;
+};
+
+WEBROOT = 'webroot'
+
+var httpServer = require('http').createServer(function(req, res) {
+  var ip = req.connection.remoteAddress;
+  paperboy
+    .deliver(WEBROOT, req, res)
+    .addHeader('Expires', 300)
+    .addHeader('X-PaperRoute', 'Node')
+    .before(function() {
+      console.log('Received Request');
+    })
+    .after(function(statCode) {
+      log(statCode, req.url, ip);
+    })
+    .error(function(statCode, msg) {
+      res.writeHead(statCode, {'Content-Type': 'text/plain'});
+      res.end("Error " + statCode);
+      log(statCode, req.url, ip, msg);
+    })
+    .otherwise(function(err) {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end("Error 404: File not found");
+      log(404, req.url, ip, err);
+    });
+});
+function log(statCode, url, ip, err) {
+  var logStr = statCode + ' - ' + url + ' - ' + ip;
+  if (err)
+    logStr += ' - ' + err;
+  console.log(logStr);
+}
+httpServer.listen(8001);
+
+var everyone = nowjs.initialize(httpServer);
+
+// TODO - clean up users & channels
+var nick_user = {};
+var clientId_user = {};
+function User(nick, clientId) {
+	this.nick = nick;
+	this.clientId = clientId;
+
+	this.channel = null;
+	function part() {
+		if(that.channel) {
+			that.channel.removeUser(that);
+		}
+	}
+	this.join = function(channel) {
+		part();
+		that.channel = channel;
+		channel.addUser(that);
+	};
+	this.destroy = function() {
+		part();
+		delete nick_user[this.nick];
+		delete clientId_user[this.clientId];
+	};
+
+	this.admin = false;
+	this.setAdmin = function(isAdmin) {
+		this.admin = isAdmin;
+	};
+
+	var that = this;
+}
+function getUser(nick, clientId) {
+	var user = nick_user[nick];
+	if(!user) {
+		user = new User(nick, clientId);
+		nick_user[nick] = user;
+		clientId_user[clientId] = user;
+	}
+	if(user.clientId != clientId) {
+		// TODO - comment!
+		user.destroy();
+		user = getUser(nick, clientId);
+	}
+	assert.equal(user.clientId, clientId);
+	return user;
+}
+
+var channels = {};
+function Channel(channelName) {
+	var users = {};
+	var adminUser = null;
+	this.sendMemberList = function() {
+		that.getGroup().now.handleChannelMembers(users);
+	};
+	this.addUser = function(user) {
+		users[user.nick] = user;
+		if(!adminUser) {
+			adminUser = user;
+			user.setAdmin(true);
+		}
+		// Add this user to the underlying nodejs group...
+		that.getGroup().addUser(user.clientId);
+		// and notify everyone in the channel of the new members list
+		this.sendMemberList();
+	};
+	this.removeUser = function(user) {
+		delete users[user.nick];
+
+		var userNames = keys(users);
+		if(user == adminUser) {
+			user.setAdmin(false);
+			// TODO - should choose next oldest User!
+			adminUser = users[userNames[0]];
+			adminUser.setAdmin(true);
+		}
+
+		// Remove this user from the underlying nodejs group...
+		that.getGroup().removeUser(user.clientId);
+		// and notify everyone in the channel of the new members list
+		this.sendMemberList();
+
+		if(userNames.length == 0) {
+			// This channel is now empty, so we can delete it
+			delete channels[channelName];
+		}
+	};
+
+	this.getGroup = function() {
+		return nowjs.getGroup(channelName);
+	};
+
+	var that = this;
+}
+function getChannel(channelName) {
+	var channel = channels[channelName];
+	if(!channel) {
+		channel = new Channel(channelName);
+		channels[channelName] = channel
+	}
+	return channel;
+}
+
+everyone.now.joinChannel = function(nick, channelName, callback) {
+	var user = getUser(nick, this.user.clientId);
+	var channel = getChannel(channelName);
+	user.join(channel); // TODO - can this fail?
+	callback();
+};
+
+everyone.now.setGameInfo = function(nick, gameInfo, callback) {
+	var user = nick_user[nick];
+	if(!user) {
+		callback("User " + nick + " not found");
+		return;
+	}
+	if(!user.admin) {
+		callback("User " + nick + " not admin");
+		return;
+	}
+
+	var channel = user.channel;
+	channel.getGroup().now.handleGameInfo(gameInfo);
+};
+
+nowjs.on('connect', function() {
+  console.log("Joined: " + this.user.clientId);
+});
+
+
+nowjs.on('disconnect', function(){
+  var user = clientId_user[this.user.clientId];
+  user.destroy();
+  console.log("Left: " + this.user.clientId);
+});
---
+++ webroot/GameMaster.js
@@ -0,0 +1,252 @@
+var GameMaster = {};
+
+(function() {
+
+var games = {};
+GameMaster.getGames = function() {
+	return games;
+};
+GameMaster.addGame = function(game) {
+	assert(!(game.gameName in games));
+
+	games[game.gameName] = game;
+};
+GameMaster.GameMaster = function() {
+	// TODO - it might be cleaner if there was a clear divide between server
+	// communication & gui stuff
+
+	var gamesDiv;
+	var inspectionCountdownDiv;
+	// TODO - could the admin configure inspection time?
+	var adminInfoDiv;
+	var gameDropdown;
+	var scrambleButton;
+	var inspectionSecondsField;
+	var myself;
+	function refreshAdminInfo() {
+		assert(myself);
+		if(myself.admin) {
+			gameDropdown.disabled = false;	
+			inspectionSecondsField.disabled = false;	
+			$(scrambleButton).show();
+		} else {
+			gameDropdown.disabled = true;	
+			inspectionSecondsField.disabled = true;	
+			$(scrambleButton).hide();
+		}
+		gameDropdown.value = gameInfo.gameName;
+		inspectionSecondsField.value = gameInfo.inspectionSeconds;
+	}
+	function refreshBoards() {
+		// cleaning up old games
+		for(var memberName in gameInstances) {
+			var gameInstance = gameInstances[memberName];
+			if(!(memberName in members) || gameInstance.game.getName() != gameInfo.gameName) {
+				// This game instance is either the wrong type of game, or is for a member
+				// who has left, so we can delete it.
+				gamesDiv.removeChild(gameInstance.div);
+				delete gameInstances[memberName];
+			}
+		}
+		// creating new games
+		for(var memberName in members) {
+			if(memberName in gameInstances) {
+				assert(gameInstances[memberName].game.getName() == gameInfo.gameName);
+			} else {
+				var babyGame = new game(moveApplied);
+				var babbyDiv = babyGame.getDiv();
+				var containerDiv = document.createElement('div');
+				containerDiv.appendChild(babbyDiv);
+				var nameDiv = document.createElement('div');
+				nameDiv.appendChild(document.createTextNode(memberName));
+				containerDiv.appendChild(nameDiv);
+				gameInstances[memberName] = { game: babyGame, div: containerDiv, nameDiv: nameDiv };
+				gamesDiv.appendChild(containerDiv);
+			}
+			var nameDiv = $(gameInstances[memberName].nameDiv);
+			if(members[memberName].admin) {
+				nameDiv.removeClass('nonAdminname');
+				nameDiv.addClass('adminName');
+			} else {
+				nameDiv.addClass('nonAdminname');
+				nameDiv.removeClass('adminName');
+			}
+		}
+	}
+
+	this.render = function(element) {
+		element.appendChild(adminInfoDiv);
+		element.appendChild(gamesDiv);
+		element.appendChild(inspectionCountdownDiv);
+	};
+	gamesDiv = document.createElement('div');
+	inspectionCountdownDiv = document.createElement('div');
+	inspectionCountdownDiv.id = 'inspection';
+	adminInfoDiv = document.createElement('div');
+
+	gameDropdown = document.createElement('select');
+	for(var gameName in games) {
+		var game = games[gameName];
+		var option = document.createElement('option');
+		option.value = gameName;
+		option.appendChild(document.createTextNode(gameName));
+		gameDropdown.appendChild(option);
+	}
+	gameDropdown.addEventListener('change', function(e) {
+		gameSelected(gameDropdown.value);
+	});
+	adminInfoDiv.appendChild(gameDropdown);
+
+	inspectionSecondsField = document.createElement('input');
+	inspectionSecondsField.type = 'number';
+	inspectionSecondsField.min = 0;
+	inspectionSecondsField.value = '15'; // TODO - where should this go?
+	inspectionSecondsField.value = '2'; // TODO - where should this go?
+	inspectionSecondsField.addEventListener('change', function(e) {
+		gameInfo.inspectionSeconds = inspectionSecondsField.value;
+		now.handleGameInfo(gameInfo); // TODO -this should be done by the server!
+	});
+	$(inspectionSecondsField).change(function() {
+		that.sendGameInfo();
+	});
+	adminInfoDiv.appendChild(inspectionSecondsField);
+
+	scrambleButton = document.createElement('input');
+	scrambleButton.value = 'Scramble!';
+	scrambleButton.type = 'button';
+	scrambleButton.addEventListener('click', function(e) {
+		var scramble = gameInstances[USERNAME].game.generateScramble();
+		GameClient.sendScramble(scramble);
+	});
+	adminInfoDiv.appendChild(scrambleButton);
+
+	function gameSelected(gameName) {
+		assert(gameName in games);
+
+		var game = games[gameName];
+		gameInfo.gameName = gameName;
+		that.sendGameInfo();
+	}
+
+	var startstamp = 0;
+	function refreshInspection() {
+		$(inspectionCountdownDiv).show();
+		var secondsUsed = parseInt((new Date().getTime() - inspectionStart)/1000);
+		var timeRemaining = gameInfo.inspectionSeconds - secondsUsed;
+		$(inspectionCountdownDiv).text(timeRemaining);
+		var topCoord = $(window).height()/2 - $(inspectionCountdownDiv).height()/2;
+		var leftCoord = $(window).width()/2 - $(inspectionCountdownDiv).width()/2;
+		$(inspectionCountdownDiv).offset({top:topCoord, left:leftCoord});
+		if (timeRemaining > 0) {
+			setTimeout(refreshInspection, 100);
+		} else {
+			startstamp = new Date().getTime();
+			$(inspectionCountdownDiv).hide();
+			gameInstances[USERNAME].game.endInspection();
+		}
+	}
+
+	function moveApplied(m) {
+		GameClient.sendMove(m, startstamp);
+	}
+
+	// Stuff the admin sets
+	var gameInfo = {};
+	var game;
+	now.handleGameInfo = function(gameInfo_) {
+		// gameInfo should have a gameName attribute and an inspectionSeconds attribute
+		gameInfo = gameInfo_;
+		assert(gameInfo.gameName in games);
+		game = games[gameInfo.gameName];
+		refreshAdminInfo();
+		refreshBoards();
+	};
+	var inspectionStart;
+	this.handleScramble = function(scramble) {
+		for(var memberName in gameInstances) {
+			gameInstances[memberName].game.setScramble(scramble);
+		}
+		inspectionStart = new Date().getTime();
+		refreshInspection();
+	};
+
+	// Stuff anyone sets
+	var gameInstances = {};
+	var members;
+	now.handleChannelMembers = function(members_) {
+		members = members_;
+		if(username in members) {
+			connected = true;
+			document.title = username + "@" + channel;
+		} else {
+			connected = false;
+			document.title = "unconnected";
+			// TODO - periodic retry?
+			// how do you catch nowjs timeouts?
+			return;
+		}
+		myself = members[username];
+
+		if(myself.admin) {
+			// Whenever someone joins the channel, we spam
+			// everyone with the game info.
+			that.sendGameInfo();
+		}
+
+		// we can't create the games until we know what game we're playing
+		var game = games[gameInfo.gameName];
+		if(!game) {
+			return;
+		}
+
+		refreshBoards();
+		refreshAdminInfo();
+	};
+	
+	this.handleMove = function(msg) {
+		assert(msg.nick in gameInstances);
+		var gameInstance = gameInstances[msg.nick];
+		var startstamp = msg.data.startstamp;
+		if(msg.nick != USERNAME) {
+			console.log(msg);
+			gameInstance.game.applyMove(msg.data.move);
+		}
+		if(gameInstance.game.isFinished()) {
+			var totalTime = (msg.timestamp - startstamp)/1000;
+			$(gameInstance.nameDiv).text(msg.nick + " " + totalTime.toFixed(2) + " seconds");
+		}
+	};
+
+	this.handleChat = function(msg) {
+	};
+
+	this.sendGameInfo = function() {
+		var gameInfo = { gameName: gameDropdown.value, inspectionSeconds: parseInt(inspectionSecondsField.value, 10) };
+		now.setGameInfo(username, gameInfo, function(error) {
+			if(error) {
+				console.log(error);
+			}
+		});
+	};
+
+	var username, channel;
+	var connected = false;
+	function setConnected(connected_) {
+		connected = connected_;
+	}
+	this.connect = function(username_, channel_) {
+		username = username_;
+		channel = channel_;
+		document.title = 'Connecting to ' + channel + '...';
+		now.joinChannel(username, channel, function(error) {
+			if(error) {
+				alert(error);
+			}
+		});
+	};
+
+
+	var that = this;
+};
+
+})();
