a17f986e6582d4622bd516a19ff1a948ff9e7277
Jeremy Fleischman <jeremyfleischman@gmail.com>
2011-11-30 05:32:16 -0800

    Updated source include. Enabling websockets!

--- newserver.js
+++ newserver.js
@@ -39,9 +39,8 @@ function log(statCode, url, ip, err) {
 httpServer.listen(8001);
 
 // TODO - i've disabled websockets because they're making chrome crash...
-// TODO - i've disabled websockets because nginx doesn't support them
-//var everyone = nowjs.initialize(httpServer);
-var everyone = nowjs.initialize(httpServer, {socketio: {transports: ['xhr-polling', 'jsonp-polling']}});
+var everyone = nowjs.initialize(httpServer);
+//var everyone = nowjs.initialize(httpServer, {socketio: {transports: ['xhr-polling', 'jsonp-polling']}});
 
 function UserSet() {
 	this.nick_user = {};
